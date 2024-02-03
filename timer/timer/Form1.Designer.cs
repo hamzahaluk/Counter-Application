a8069eb266e1f86f44cd65240950ac2830cc9123
hamzahaluk <hamzasonmez91@gmail.com>
2024-02-03 11:57:29 +0300

    Counter/Timer Application
    In this application, a forward-backward counter example is made.

---
+++ timer/timer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_basla = new System.Windows.Forms.Button();
+            this.btn_dur = new System.Windows.Forms.Button();
+            this.btn_sifirla = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(190, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btn_basla
+            // 
+            this.btn_basla.Location = new System.Drawing.Point(190, 175);
+            this.btn_basla.Name = "btn_basla";
+            this.btn_basla.Size = new System.Drawing.Size(75, 23);
+            this.btn_basla.TabIndex = 1;
+            this.btn_basla.Text = "Başlat";
+            this.btn_basla.UseVisualStyleBackColor = true;
+            this.btn_basla.Click += new System.EventHandler(this.btn_basla_Click);
+            // 
+            // btn_dur
+            // 
+            this.btn_dur.Location = new System.Drawing.Point(285, 175);
+            this.btn_dur.Name = "btn_dur";
+            this.btn_dur.Size = new System.Drawing.Size(75, 23);
+            this.btn_dur.TabIndex = 2;
+            this.btn_dur.Text = "Dur";
+            this.btn_dur.UseVisualStyleBackColor = true;
+            this.btn_dur.Click += new System.EventHandler(this.btn_dur_Click);
+            // 
+            // btn_sifirla
+            // 
+            this.btn_sifirla.Location = new System.Drawing.Point(377, 175);
+            this.btn_sifirla.Name = "btn_sifirla";
+            this.btn_sifirla.Size = new System.Drawing.Size(75, 23);
+            this.btn_sifirla.TabIndex = 3;
+            this.btn_sifirla.Text = "Sıfırla";
+            this.btn_sifirla.UseVisualStyleBackColor = true;
+            this.btn_sifirla.Click += new System.EventHandler(this.btn_sifirla_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 340);
+            this.Controls.Add(this.btn_sifirla);
+            this.Controls.Add(this.btn_dur);
+            this.Controls.Add(this.btn_basla);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_basla;
+        private System.Windows.Forms.Button btn_dur;
+        private System.Windows.Forms.Button btn_sifirla;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ timer/timer/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int saat = 0, dakika = 0, saniye = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = saat.ToString().PadLeft(2, '0') + ":" + dakika.ToString().PadLeft(2, '0') + ":" + saniye.ToString().PadLeft(2, '0');
+            saniye++;
+            if (saniye==60)
+            {
+                dakika++;
+                saniye = 0;
+                    if (dakika==60)
+	                {
+                        saat++;
+                        dakika = 0;
+                        if (saat==24)
+                        {
+                            saat = 0;
+                        }
+	                }
+            }
+         
+
+        }
+
+        private void btn_basla_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void btn_dur_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void btn_sifirla_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            saat = 0;
+            dakika = 0;
+            saniye = 0;
+            textBox1.Text = saat.ToString().PadLeft(2, '0') + ":" + dakika.ToString().PadLeft(2, '0') + ":" + saniye.ToString().PadLeft(2, '0');
+        }
+    }
+}
